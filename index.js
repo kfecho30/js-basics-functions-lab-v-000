366d0853d3dd858219d4ba8cc083a5424f475b53
Kelsey Fecho <kfecho30@gmail.com>
2018-06-16 17:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(dest) {
   if(dest >= 42) {
-    return dest-42
+    return dest-42;
   } else {
-    return 42-dest
+    return 42-dest;
   }
 }
 
 function distanceFromHqInFeet(dest) {
-  return distanceFromHqInBlocks(dest) * 264
+  return distanceFromHqInBlocks(dest) * 264;
 }
 
 function distanceTravelledInFeet(dest1, dest2) {
   if(dest1 >= dest2) {
-    return (dest1-dest2)*264
+    return (dest1-dest2)*264;
   } else {
-    return (dest2-dest1)*264
+    return (dest2-dest1)*264;
   }
+}
+
+function calculatesFarePrice(start, end){
+  let dist = distanceTravelledInFeet(start, end);
+  if (dist <= 400) {
+    return "This one is one us!"
+  } else if (distanceTrav)
 }
