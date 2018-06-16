1441777202bf7d82bee811d6d1ed6179c0f1e524
Kelsey Fecho <kfecho30@gmail.com>
2018-06-16 17:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function distanceFromHqInBlocks(dest) {
 function distanceFromHqInFeet(dest) {
   return distanceFromHqInBlocks(dest) * 264
 }
+
+function distanceTravelledInFeet(dest1, dest2) {
+  if(des1t >= dest2) {
+    return dest-dest2
+  } else {
+    return dest2-dest1
+  }
+}
