1436bc1ee7d8035efa2bb3f826a91094995f118b
Kelsey Fecho <kfecho30@gmail.com>
2018-06-16 18:02:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,9 @@ function calculatesFarePrice(start, end){
     return 0;
   } else if (dist <= 2000) {
     return (dist-400)*.02;
+  } else if (dist <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
 }
